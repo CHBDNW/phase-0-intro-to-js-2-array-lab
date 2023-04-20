874d73456a92c20a35de08926e1c932b7b75d92b
+ <+>
2023-04-20 12:38:01 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat){
+
+cats.push("Ralph");
+};
+function destructivelyPrependCat(cat){
+    cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat(cat){
+    cats.pop(); 
+};
+function destructivelyRemoveFirstCat(cat){
+    cats.shift();
+};
+function appendCat(name) {
+    const newCats = [...cats, "Broom"]; 
+   return newCats 
+};
+function prependCat(name) {
+    const newCats2 = ["Arnold",...cats]; 
+   return newCats2 
+};
+function removeLastCat() {
+    const newCats = cats.slice(0, -1); 
+    return newCats; 
+  };
+  function removeFirstCat() {
+    const newCats = cats.slice(1); 
+    return newCats; 
+  };
+  
+     
